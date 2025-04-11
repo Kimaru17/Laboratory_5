b8c29a9d69caa2bf0097617b278640fb3782b527
Varelexx <jeferson.varela@ucr.ac.cr>
2025-04-11 14:34:20 -0600

    Staffin

---
+++ src/main/java/controller/StaffingController.java
@@ -0,0 +1,64 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+
+public class StaffingController
+{
+    @javafx.fxml.FXML
+    private TableView employeeTableview;
+    @javafx.fxml.FXML
+    private TableColumn idTableColumn;
+    @javafx.fxml.FXML
+    private BorderPane bp;
+    @javafx.fxml.FXML
+    private TableColumn supervisorTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn employeeIdTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn jobPositionTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn assignmentTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn dateTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn employeeNameTableColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sizeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sortByEmpIdOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sortByEmpNameOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sortByJobOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sortByTypeOnAction(ActionEvent actionEvent) {
+    }
+}
